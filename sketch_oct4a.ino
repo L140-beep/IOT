97793981a828aecaf35fc1b71573e2fb3d577753
Maxim Klypin <70793854+L140-beep@users.noreply.github.com>
2022-10-06 20:22:39 +0800

    Update sketch_oct4a.ino

--- sketch_oct4a.ino
+++ sketch_oct4a.ino
@@ -1,12 +1,12 @@
 #define btn_pin 12
 #define led_pin 3
 #define check_time 20
-#define size 100
 
 long prev = 0;
-int arr[size];
-int counter = 0;
 bool flag = false;
+bool is_full_brightness = false;
+bool is_btn_pressed = false;
+int brightness = 0;
 
 void setup() {
   pinMode(led_pin, OUTPUT);
@@ -16,19 +16,45 @@ void setup() {
 
 void loop(){
   int val = digitalRead(btn_pin);
-  if (val == 1){    
+
+  if(val == 0){
     if(millis() - prev > check_time){
-      flag = !flag;
-      Serial.println("PRESSED!");
+      is_btn_pressed = true;
     }
+  } else {
+    is_btn_pressed = false;
     prev = millis();
   }
 
-  if(flag){
-    digitalWrite(led_pin, HIGH);
-  }
-  else{
-    digitalWrite(led_pin, LOW);        
-  }
+  if(is_btn_pressed){
+    if (millis() - prev > 300){
+        
+        Serial.println("PRESSED!");
 
+        if(brightness < 255 && !is_full_brightness){
+          brightness += 1;
+        }else{
+          brightness -= 1;
+        }
+        if(brightness == 255){
+          is_full_brightness = true;
+        }
+        if(brightness == 0){
+          is_full_brightness = false;
+        }
+
+        analogWrite(led_pin, brightness);
+    } else{
+      Serial.println("Click");
+      flag = !flag;
+    }
+  }else{
+    brightness = 0;
+    
+    if(flag){
+      digitalWrite(led_pin, HIGH);
+    } else{
+      digitalWrite(led_pin, LOW);      
+    }
+  }
 }
